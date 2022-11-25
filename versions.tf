a6e3b10b3a9b5373774846710bc185175f5f8e14
Richard Slaughter <richardslaughter@DESKTOP-FFFB0SU.localdomain>
2022-11-25 23:47:26 +0000

    tweaking vars

--- versions.tf
+++ versions.tf
@@ -2,14 +2,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.0"
+      version = "~> 4.0"
     }
   }
 
-  required_version = ">= 0.14.0"
+  required_version = ">= 1.0"
 }
