206de7effc971d414effd63d0d15d13e1d45d909
Richard Slaughter <richardslaughter@DESKTOP-FFFB0SU.localdomain>
2022-11-26 22:21:07 +0000

    tweaks for ssm

--- main.tf
+++ main.tf
@@ -22,13 +22,12 @@ data "aws_ami" "al2" {
 
 module "example_instance" {
   source  = "app.terraform.io/octelle/module-ec2/tf"
-  version = "~> 1.0.1"
+  version = "~> 1.0.2"
 
-  name          = var.instance_name
-  instance_type = var.instance_type
-  subnet_id     = data.aws_subnets.private_subnets.ids[0]
-  ami           = data.aws_ami.al2.id
-
-  user_data     = file("${path.module}/templates/user_data")
+  name            = var.instance_name
+  instance_type   = var.instance_type
+  subnet_id       = data.aws_subnets.private_subnets.ids[0]
+  ami             = data.aws_ami.al2.id
+  user_data       = file("${path.module}/templates/user_data")
 }
 
