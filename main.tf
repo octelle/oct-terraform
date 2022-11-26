31146883b5e8dd963ab1f48f4bba0dd0d30592a3
Richard Slaughter <richardslaughter@Richards-Air.localdomain>
2022-11-26 17:37:18 +0000

    missed something

--- main.tf
+++ main.tf
@@ -8,14 +8,17 @@ provider "aws" {
   }
 }
 
-data "aws_ami" "ami" {
-  most_recent = true
+data "aws_ami" "al2" {
+  owners = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20221103.3-x86_64-gp2*"]
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
   }
+
+  most_recent = true
 }
+
 
 module "example_instance" {
   source  = "app.terraform.io/octelle/module-ec2/tf"
@@ -24,7 +27,9 @@ module "example_instance" {
   name          = var.instance_name
   instance_type = var.instance_type
   //subnet_id     = data.aws_subnets.private_subnets.ids[0]
-  subnet_id = "subnet-091ef76c2607e5010"
-  ami           = data.aws_ami.ami.id
+  subnet_id     = "subnet-091ef76c2607e5010"
+  ami           = data.aws_ami.al2.id
+
+  user_data     = file("${path.module}/templates/user_data")
 }
 
